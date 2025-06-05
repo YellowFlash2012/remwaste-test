020e57575e4d8425eb0ca45d7d901e8333f49722
YellowFlash2012 <cyclop9@hotmail.com>
2025-06-05 17:07:39 +0100

    create components and app folders in src. Design and implement the redux algo to fetch data from the provided link.

---
+++ src/app/store.js
@@ -0,0 +1,12 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/ApiSlice";
+
+export const store = configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
+});
---
+++ src/features/ApiSlice.js
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+const baseQuery = fetchBaseQuery({
+    baseUrl:
+        "https://clicks.aweber.com/y/ct/?l=eNKMKn&m=8kKiA5Xs4lyuOBlr&b=LN4zdPOCY2wffjE5vH.B0w",
+});
+
+export const apiSlice = createApi({
+    baseQuery,
+    tagTypes: ["Skip"],
+    endpoints: (builder) => ({}),
+});
