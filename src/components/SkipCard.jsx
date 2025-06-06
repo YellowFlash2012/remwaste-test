7efaf7238a3bc73864c03e11cb0fe15eb8ca5528
YellowFlash2012 <cyclop9@hotmail.com>
2025-06-06 11:17:53 +0100

    completing the UI of SkipCard component. Add the modal and the functionalities to make it dynamic.

--- src/components/SkipCard.jsx
+++ src/components/SkipCard.jsx
@@ -1,4 +1,91 @@
-const SkipCard = () => {
-    return <div>SkipCard</div>;
+import { useState } from "react";
+import { Badge, Button, ButtonGroup, Card, Modal, Stack } from "react-bootstrap";
+import { VscArrowSmallRight } from "react-icons/vsc";
+
+const SkipCard = ({ skip }) => {
+    const [modal, setModal] = useState(false);
+
+    const handleModal = () => {
+        setModal(!modal)
+    }
+
+    return (
+        <>
+            <Card className="my-3 py3 rounded" onClick={handleModal}>
+                <Card.Body>
+                    <Card.Header>
+                        <Stack direction="horizontal">
+                            <Card.Title as="div">
+                                ID: <strong>{skip?.id}</strong>
+                            </Card.Title>
+                        </Stack>
+                    </Card.Header>
+
+                    <Card.Text as="div">Size: {skip?.size}</Card.Text>
+
+                    <Card.Text
+                        as="h3"
+                        style={{ color: "blue", fontSize: "bold" }}
+                    >
+                        £ {skip?.price_before_vat}
+                    </Card.Text>
+
+                    <Card.Text as="h6">VAT: £ {skip?.vat}</Card.Text>
+
+                    <Card.Text as="h6">Post Code: {skip?.postcode}</Card.Text>
+
+                    <Badge bg="danger">{skip?.forbidden}</Badge>
+
+                    <Button
+                        className="select-skip"
+                        variant={modal ? "primary" : "secondary"}
+                        type="button"
+                        size="lg"
+                        style={{ width: "100%" }}
+                        onClick={handleModal}
+                    >
+                        {modal ? (
+                            "Selected"
+                        ) : (
+                            <>
+                                Select This Skip <VscArrowSmallRight />
+                            </>
+                        )}
+                    </Button>
+                </Card.Body>
+            </Card>
+
+            <Modal show={modal} onHide={handleModal} size="xl">
+                <Modal.Body>
+                    <h6>
+                        Imagery and information shown throughout this website
+                        may not reflect the exact shape or size specification,
+                        colours may vary, options and/or accessories may be
+                        featured at additional cost.
+                    </h6>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <div className="d-flex justify-content-between w-100">
+                        <div>
+                            {skip?.id}{" "}
+                            <span style={{ color: "blue", fontSize: "bold" }}>
+                                £{skip?.price_before_vat}
+                            </span>{" "}
+                            {skip?.hire_period_days} day hire
+                        </div>
+                        <div>
+                            <Button variant="secondary" className="me-2" onClick={handleModal}>
+                                Back
+                            </Button>
+                            <Button variant="primary">
+                                Continue <VscArrowSmallRight />
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 };
 export default SkipCard;
--- src/pages/SelectSkip.jsx
+++ src/pages/SelectSkip.jsx
@@ -1,5 +1,9 @@
+import { Col, Row } from "react-bootstrap";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 import ProgressBar from "../components/ProgressBar";
 import { useGetAllSkipsQuery } from "../features/skipSlice";
+import SkipCard from "../components/SkipCard";
 
 const SelectSkip = () => {
     const { data, isLoading, error } = useGetAllSkipsQuery();
@@ -10,9 +14,27 @@ const SelectSkip = () => {
         <>
             <ProgressBar step1 step2 step3 />
 
-            <h2 className="text-center mb-4">Choose Your Skip Size</h2>
+            {isLoading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant="danger">{error}</Message>
+            ) : (
+                <>
+                    <h2 className="text-center mb-4">Choose Your Skip Size</h2>
 
-            <h5 className="text-center">Select the skip size that best suits your needs</h5>
+                    <h5 className="text-center">
+                        Select the skip size that best suits your needs
+                    </h5>
+
+                    <Row>
+                        {data?.map((skip) => (
+                            <Col key={data?.id} sm={12} md={6} lg={4} xl={3}>
+                                <SkipCard skip={skip} />
+                            </Col>
+                        ))}
+                    </Row>
+                </>
+            )}
         </>
     );
 };
